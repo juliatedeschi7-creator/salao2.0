377bd6092bb8a2e56cc4d6feb8875aff3332e2ef
juliatedeschi7-creator <juliatedeschi7@gmail.com>
2026-07-06 23:26:21 -0300

    Criar sw.js

---
+++ public/sw.js
@@ -0,0 +1,40 @@
+self.addEventListener('push', function (event) {
+  if (!event.data) return;
+
+  const data = event.data.json();
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: data.icon || '/logo.png',
+      badge: data.badge || '/logo.png',
+      data: {
+        url: data.url || '/'
+      }
+    })
+  );
+});
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+
+  const url = event.notification.data?.url || '/';
+
+  event.waitUntil(
+    clients.matchAll({
+      type: 'window',
+      includeUncontrolled: true
+    }).then((clientList) => {
+
+      for (const client of clientList) {
+        if (client.url.includes(url) && 'focus' in client) {
+          return client.focus();
+        }
+      }
+
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
+  );
+});
