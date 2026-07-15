5f7695a21f583de0fd1ce7ce4da3c21e4da4800e
juliatedeschi7-creator <juliatedeschi7@gmail.com>
2026-07-14 23:22:33 -0300

    Atualizar o next.config.js

--- next.config.js
+++ next.config.js
@@ -6,6 +6,21 @@ const nextConfig = {
   images: {
     domains: ['uhsvfbebjfeedpeznjnf.supabase.co'],
   },
+  async headers() {
+    return [
+      {
+        // Libera o CORS para as rotas da sua API
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          // Usando "*" temporariamente para garantir que o Codespace não bloqueie os testes
+          { key: "Access-Control-Allow-Origin", value: "*" }, 
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
