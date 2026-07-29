9ab47442d3b81e4007a696bf590af51e3ae03e16
juliatedeschi7-creator <juliatedeschi7@gmail.com>
2026-07-29 07:34:03 -0300

    Atualizar o push-client.ts

--- src/lib/push-client.ts
+++ src/lib/push-client.ts
@@ -68,7 +68,7 @@ export async function registrarPush(profileId: string): Promise<boolean> {
     if (!subscription) {
       subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(vapidPublicKey) as unknown as Uint8Array
+        applicationServerKey: urlBase64ToUint8Array(vapidPublicKey) as BufferSource
       })
     }
 
