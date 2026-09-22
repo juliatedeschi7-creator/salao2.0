39aa0b87065edbb26eec179982d4db1e4f8297b5
juliatedeschi7-creator <juliatedeschi7@gmail.com>
2026-09-22 12:15:38 -0300

    Atualizar o route.ts

--- src/app/api/lembretes-agendamento/automatico/route.ts
+++ src/app/api/lembretes-agendamento/automatico/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export const dynamic = 'force-dynamic'
 export const runtime = 'nodejs'
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
   const inicio = new Date()
 
   console.log('================================================')
